badbf0b5eb717a8345ed906e2811e7c1cd012e16
AleLincoln <50921123+AleLincoln@users.noreply.github.com>
2021-02-28 17:10:44 +0000

    correct the useName variable

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,4 +1,5 @@
 
+import { useRouter } from 'next/router'
 import { useContext } from 'react'
 import { ChallengesContext } from '../contexts/ChallengesContext'
 import styles from '../styles/components/Profile.module.css'
@@ -6,9 +7,10 @@ import styles from '../styles/components/Profile.module.css'
 export default function Profile(){
 
     const {level, userName} = useContext(ChallengesContext)
+
     return (
         <div className={styles.profileContainer}>
-            <img src={`https://github.com/${userName}.png`} alt={userName}></img>
+            <img src={`https://github.com/${userName}.png`}></img>
             <div>
                 <strong>
                     {userName}    
